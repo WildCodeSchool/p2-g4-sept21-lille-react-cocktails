1a423bdbcbe9dec1b28b798e3df7a5b78ea135bc
Pierre Goemans <p.goemans@hotmail.fr>
2021-10-26 16:32:22 +0200

    Add use effect for fix loop

--- src/component/CocktailCard.jsx
+++ src/component/CocktailCard.jsx
@@ -1,20 +1,23 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function CocktailCard() {
   const [cocktails, setCocktails] = useState([]);
-  axios
-    .get('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
-    .then(({ data }) => {
-      setCocktails(data.drinks);
-    });
+
+  useEffect(() => {
+    axios
+      .get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s')
+      .then(({ data }) => {
+        setCocktails(data.drinks);
+      });
+  }, []);
+
   return (
     <div>
       <ul>
         {' '}
         {cocktails.map((result) => {
-          console.log(result);
-          return <li>{result.strCategory}</li>;
+          return <img src={result.strDrinkThumb} alt="" />;
         })}
       </ul>
     </div>
