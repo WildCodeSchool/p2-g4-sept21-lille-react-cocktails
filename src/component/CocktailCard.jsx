c08aef8604ead05ab1f17913ff8b38702bea64e0
Pierre Goemans <p.goemans@hotmail.fr>
2021-10-26 15:57:09 +0200

    First config with API

--- src/component/CocktailCard.jsx
+++ src/component/CocktailCard.jsx
@@ -1,3 +1,22 @@
+import axios from 'axios';
+import { useState } from 'react';
+
 export default function CocktailCard() {
-  return 1;
+  const [cocktails, setCocktails] = useState([]);
+  axios
+    .get('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
+    .then(({ data }) => {
+      setCocktails(data.drinks);
+    });
+  return (
+    <div>
+      <ul>
+        {' '}
+        {cocktails.map((result) => {
+          console.log(result);
+          return <li>{result.strCategory}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
