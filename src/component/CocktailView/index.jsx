9b73b87e600e52e796d9254de0b8afe5c768954d
Morel Victor <morel.victor1@gmail.com>
2021-10-27 12:27:49 +0200

    import API ok in  cocktailView

---
+++ src/component/CocktailView/index.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+// import CocktailCard from '../CocktailCard';
+
+export default function CocktailView() {
+  const [cocktails, setCocktails] = useState([]);
+  // const [result, setResult] = useState();
+
+  useEffect(() => {
+    axios
+      .get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s')
+      .then(({ data }) => {
+        setCocktails(data.drinks);
+      });
+  }, []);
+
+  console.log(cocktails);
+  return (
+    <div>
+      <ul>
+        {cocktails.map((result) => {
+          return <li>{result.idDrink}</li>;
+          // return <CocktailCard result={result} />;
+        })}
+      </ul>
+    </div>
+  );
+}
