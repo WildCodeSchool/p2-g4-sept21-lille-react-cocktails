48e628fe81ad591f716f29224d2197dbe83495df
HeloiseFerla <ferla.heloise@gmail.com>
2021-10-27 17:29:18 +0200

    Add a header with SearchBar and Favorites components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Header from './components/Header';
+
+import './style.css';
+
 function App() {
   return (
-  <>
-  <Header/>
-  </>
+    <>
+      <Header displaySearch />
+    </>
   );
 }
 
