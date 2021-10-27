09637f5b99b2fe7f0d72265b37ce0e27476dd2ce
Morel Victor <morel.victor1@gmail.com>
2021-10-27 16:57:18 +0200

    Function for filter key of props

--- src/component/CocktailCard/index.jsx
+++ src/component/CocktailCard/index.jsx
@@ -1,12 +1,40 @@
 import PropTypes from 'prop-types';
+// import { useEffect } from 'react';
 
-export default function CocktailCard({ strDrink }) {
+export default function CocktailCard({ strDrink, strDrinkThumb, ...rest }) {
+  // useEffect(() => {
+  const listing = Object.keys(rest)
+    .filter((key) => {
+      return key.includes('Ingredient') && rest[key] !== null;
+    })
+    .map((key) => {
+      return rest[key];
+    })
+    .join(', ');
+  // }, []);
+
   return (
     <div>
-      <div>{strDrink}</div>
+      <div>
+        <div className="mainCard">
+          <div className="imgCard">
+            <img src={strDrinkThumb} alt={strDrink} />
+          </div>
+          <h2>{strDrink}</h2>
+          <div className="tagIcon">{listing}</div>
+        </div>
+      </div>
     </div>
   );
 }
 
-CocktailCard.propTypes = { strDrink: PropTypes.string };
-CocktailCard.defaultProps = { strDrink: '' };
+CocktailCard.propTypes = {
+  strDrink: PropTypes.string,
+  strDrinkThumb: PropTypes.string,
+  strIngredients: PropTypes.string,
+};
+CocktailCard.defaultProps = {
+  strDrink: '',
+  strDrinkThumb: '',
+  strIngredients: '',
+};
