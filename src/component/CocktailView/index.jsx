e404522760c49cc863c9ac25d45ad9239b39e84b
Morel Victor <morel.victor1@gmail.com>
2021-10-28 11:01:32 +0200

    Modify css in CocktailCard and CocktailView

--- src/component/CocktailView/index.jsx
+++ src/component/CocktailView/index.jsx
@@ -17,6 +17,9 @@ export default function CocktailView() {
 
   return (
     <div>
+      <div className="fakeHeader">
+        <p>HEADER</p>
+      </div>
       <div className="cardContainer">
         {cocktails.map((data) => {
           return <CocktailCard key={data.idDrink} {...data} />;
